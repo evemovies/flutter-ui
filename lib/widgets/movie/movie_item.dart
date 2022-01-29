02da4cb63b424ba84b409fe356fcdb806c641699
Dmitrii Baranov <dmitrii@skyselect.com>
2022-01-29 16:05:46 +0200

    Fixed movies list bottom padding, added simple animation for movies list

--- lib/widgets/movie/movie_item.dart
+++ lib/widgets/movie/movie_item.dart
@@ -4,8 +4,9 @@ import 'package:eve_mobile/views/movie_view.dart';
 
 class MovieItem extends StatelessWidget {
   final Movie movie;
+  final bool animate;
 
-  const MovieItem({Key? key, required this.movie}) : super(key: key);
+  const MovieItem({Key? key, required this.movie, required this.animate}) : super(key: key);
 
   void _handleMovieTap(BuildContext context) {
     Navigator.pushNamed(context, MovieView.routeName, arguments: MovieViewRouteArgs(movieId: movie.id));
@@ -15,15 +16,16 @@ class MovieItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => _handleMovieTap(context),
-      child: Container(
-        child: Column(children: [
-          Image.network(
-            movie.posterUrl,
-            height: 350,
-          ),
-          Text(movie.title, overflow: TextOverflow.ellipsis)
-        ]),
-      ),
+      child: AnimatedContainer(
+          margin: EdgeInsetsDirectional.only(top: animate ? 0 : 200),
+          duration: Duration(milliseconds: 500),
+          child: Column(children: [
+            Image.network(
+              movie.posterUrl,
+              height: 350,
+            ),
+            Text(movie.title, overflow: TextOverflow.ellipsis)
+          ])),
     );
   }
 }
--- lib/widgets/movie/movies_list.dart
+++ lib/widgets/movie/movies_list.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:eve_mobile/models/movie_model.dart';
 import 'package:eve_mobile/widgets/movie/movie_item.dart';
 
-class MoviesList extends StatelessWidget {
+class MoviesList extends StatefulWidget {
   final List<Movie> moviesList;
   final Future<void> Function() onMovieRefresh;
   final String? emptyMessage;
@@ -12,13 +12,31 @@ class MoviesList extends StatelessWidget {
       {Key? key, required this.moviesList, required this.onMovieRefresh, this.emptyMessage, this.errorMessage})
       : super(key: key);
 
+  @override
+  _MoviesListState createState() => _MoviesListState();
+}
+
+class _MoviesListState extends State<MoviesList> {
+  bool animate = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration.zero, () {
+      setState(() {
+        animate = true;
+      });
+    });
+  }
+
   List<List<Movie>> _getMoviePairs() {
     List<List<Movie>> result = [];
 
-    for (var i = 0; i < moviesList.length; i += 2) {
-      var end = i + 2 > moviesList.length ? moviesList.length : i + 2;
+    for (var i = 0; i < widget.moviesList.length; i += 2) {
+      var end = i + 2 > widget.moviesList.length ? widget.moviesList.length : i + 2;
 
-      result.add(moviesList.sublist(i, end));
+      result.add(widget.moviesList.sublist(i, end));
     }
 
     return result;
@@ -27,39 +45,42 @@ class MoviesList extends StatelessWidget {
   _renderMessage() {
     var defaultMessage = 'No movies to display';
 
-    return Center(child: Text(errorMessage ?? emptyMessage ?? defaultMessage));
+    return Center(child: Text(widget.errorMessage ?? widget.emptyMessage ?? defaultMessage));
   }
 
   @override
   Widget build(BuildContext context) {
     var pairs = _getMoviePairs();
 
-    if (moviesList.length == 0) return _renderMessage();
+    if (widget.moviesList.length == 0) return _renderMessage();
 
-    return CustomScrollView(
-      physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-      slivers: [
-        CupertinoSliverRefreshControl(onRefresh: onMovieRefresh),
-        SliverList(
-            delegate: SliverChildBuilderDelegate((context, index) {
-          var children = pairs[index].asMap().entries.map((e) {
-            return Expanded(
-                child: Padding(
-              padding: EdgeInsets.all(8),
-              child: MovieItem(
-                movie: pairs[index][e.key],
-              ),
-            ));
-          }).toList();
+    return Padding(
+        padding: EdgeInsets.only(bottom: 100),
+        child: CustomScrollView(
+          physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+          slivers: [
+            CupertinoSliverRefreshControl(onRefresh: widget.onMovieRefresh),
+            SliverList(
+                delegate: SliverChildBuilderDelegate((context, index) {
+              var children = pairs[index].asMap().entries.map((e) {
+                return Expanded(
+                    child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: MovieItem(
+                    movie: pairs[index][e.key],
+                    animate: animate,
+                  ),
+                ));
+              }).toList();
 
-          // Imitate one more slot
-          if (children.length == 1) children.add(Expanded(child: Container()));
+              // Imitate one more slot
+              if (children.length == 1) children.add(Expanded(child: Container()));
 
-          return Row(
-            children: children,
-          );
-        }, childCount: pairs.length))
-      ],
-    );
+              return Row(
+                children: children,
+              );
+            }, childCount: pairs.length))
+          ],
+        ));
   }
 }
